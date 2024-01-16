78a0a94091f908d0d24329752d7ead7c9b88f5bf
Marina <marina.moger@gmail.com>
2024-01-16 11:30:56 +0200

    for loops

---
+++ src/ForLoopExample3.java
@@ -0,0 +1,38 @@
+/*
+Learn Programming Academy's Java 1Z0-815 Certification Exam Course
+Section 4: Using Operators and Decision Constructs
+Topic: Loops
+Sub-Topic: For Loop
+*/
+
+public class ForLoopExample3 {
+
+    // Create a method which may throw an exception and returns an int
+    public static int interruptIt(int i) throws Exception {
+        System.out.println("interruptIt: i = " + i);
+        if (i < 10) throw new Exception("Break it up");
+        return i;
+    }
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        // First, let's throw an exception in the initialization block...
+        try {
+            System.out.println("------- Executing For Loop A -------");
+            // termination section of for loop declaration has prefix increment
+            // increment section of for loop declaration uses postfix increment
+            /*for (interruptIt(i); ++i < 10; i++) {
+                System.out.println("A forloop: i = " + i);
+            }*/
+            /*for (i = 0; interruptIt(++i) < 10; i++) {
+                System.out.println("A forloop: i = " + i);
+            }*/
+            for (i = 0; ++i < 10; interruptIt(i++)){
+                System.out.println("A forloop: i = " + i);
+            }
+        } catch (Exception e) {
+            System.out.println(e + ": i = " + i);
+        }
+    }
+}
